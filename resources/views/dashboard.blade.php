a5c146b57b1eea32f8f95dd685d0a76ffbe7c2e5
Ben Hirsch <benhirsch@gmail.com>
2025-02-16 08:30:46 -0500

    misc progress

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -17,7 +17,7 @@
         </div>
     </x-slot>
 
-    <div class="sticky z-30 bg-gray-100 dark:bg-gray-900">
+    <div class="bg-gray-100 dark:bg-gray-900">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <livewire:weight-chart :chartData="$chartData" />
         </div>
@@ -27,30 +27,15 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 h-full">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg h-full">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <div class="relative">
-                        <div class="overflow-y-auto" id="days-container" style="max-height: calc(100vh - 400px);">
-                            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 pb-14 pt-4">
+                        <div id="days-container">
+                            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-1">
                                 @foreach ($days as $day)
                                     <livewire:day-entry :day="$day" :key="$day['date']" />
                                 @endforeach
                             </div>
                         </div>
-                    </div>
                 </div>
             </div>
         </div>
     </div>
-
-    <script>
-        document.addEventListener('livewire:initialized', () => {
-            const currentDay = document.getElementById('current-day');
-            if (currentDay) {
-                const container = document.getElementById('days-container');
-                const offset = 16;
-                
-                currentDay.scrollIntoView({ behavior: 'auto', block: 'start' });
-                container.scrollTop -= offset;
-            }
-        });
-    </script>
 </x-app-layout>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,12 +21,12 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     </head>
     <body class="font-['Inter'] antialiased">
-        <div class="min-h-screen bg-gray-900 flex flex-col h-screen">
-            <livewire:layout.navigation class="sticky top-0 z-50" />
+        <div class="min-h-screen bg-gray-900 flex flex-col">
+            <livewire:layout.navigation class="" />
 
             <!-- Page Heading -->
             @if (isset($header))
-                <header class="bg-white dark:bg-gray-800 shadow sticky top-[64px] z-40">
+                <header class="bg-white dark:bg-gray-800 shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
